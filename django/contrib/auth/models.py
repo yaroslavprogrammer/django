5381328ebbc291f691bb10ad1a980b62a4546a01
Yaroslav Rudenok <yaroslavprogrammer@gmail.com>
2014-10-20 13:45:47 +0300

    Changeed username

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -381,7 +381,7 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
 
     Username, password and email are required. Other fields are optional.
     """
-    username = models.CharField(_('username'), max_length=30, unique=True,
+    username = models.CharField(u'Логин/Email', max_length=30, unique=True,
         help_text=_('Required. 30 characters or fewer. Letters, numbers and '
                     '@/./+/-/_ characters'),
         validators=[
